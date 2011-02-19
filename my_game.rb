928c4722fa7f269d64c421701b8a9576c4efa549
Klaus Hebsgaard <klaus@hebsgaard.dk>
2011-02-19 07:26:56 +0100

    game now working

--- my_game.rb
+++ my_game.rb
@@ -7,10 +7,22 @@ class MyGame < Gosu::Window
     def initialize
       super(800, 600, false)
       @player1 = Player.new(self, "images/player1_icon.png")
+
+      @cake = Player.new(self, "images/cake_icon.png")
+      @cake.y = 500
+      @cake.x = 50
+
       @dog = Player.new(self, "images/dog_icon.png")
       @dog_mower = DogMower.new(@dog)
       @dog.x = 100
       @dog.y = 300
+      @dog.pixel_movement = 10
+
+      @hurray = Player.new(self, "images/hurray.png")
+      @hurray.x = -300
+      @hurray.y = -300
+
+      @has_won = false
     end
 
     def update
@@ -38,11 +50,23 @@ class MyGame < Gosu::Window
       close
     end
 
+    if @cake.hit_by?(@player1)
+    
+      @has_won = true
+    end
+
+    if @has_won
+      @hurray.move_down
+      @hurray.move_right
+    end
+
     end
 
     def draw
       @player1.draw
       @dog.draw
+      @cake.draw
+      @hurray.draw
     end
 
 end
--- player.rb
+++ player.rb
@@ -4,12 +4,14 @@ class Player
   attr_accessor :x
   attr_reader :right_boundry
   attr_reader :bottom_boundry
+  attr_accessor :pixel_movement
 
   def initialize(game_window, icon_path)
     @game_window = game_window
     @icon = Gosu::Image.new(@game_window,  icon_path, true)
     @x = 0
     @y = 0
+    @pixel_movement = 5
     @right_boundry = @game_window.width - @icon.width
     @bottom_boundry = @game_window.height - @icon.height
 
@@ -20,25 +22,25 @@ class Player
   end
   def move_left
     if @x > 0
-      @x -= 5
+      @x -= @pixel_movement
     end
   end
 
   def move_right
     if @x < @right_boundry
-      @x += 5
+      @x += @pixel_movement
     end
   end
 
   def move_up
      if @y > 0
-      @y -= 5
+      @y -= @pixel_movement
     end
   end
 
   def move_down
     if @y < @bottom_boundry
-      @y += 5
+      @y += @pixel_movement
     end
   end
 
